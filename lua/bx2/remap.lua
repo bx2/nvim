8f844385185611636bbc67c9918ce20b06d871c9
bx2 <burclaf@gmail.com>
2022-12-26 22:21:04 +0100

    add remaps

--- lua/bx2/remap.lua
+++ lua/bx2/remap.lua
@@ -1,3 +1,37 @@
-vim.g.mapleader = ' '
+vim.g.mapleader = ','
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set("n", "Q", "<nop>")
+
+-- move selection up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- move current line / block with Alt-j/k ala vscode.
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- keep the cursor in place when jumping up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- keep cursor in place when browsing through search results
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste over visual selection
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- copy/paste to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- format buffer
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- quickfix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- replace the current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
