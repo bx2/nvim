8d76e4889d18dcb6aabb4c38c2f543a38dbc2809
bx2 <burclaf@gmail.com>
2022-12-28 08:04:33 +0100

    add git-signs

--- lua/bx2/set.lua
+++ lua/bx2/set.lua
@@ -3,6 +3,11 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = false
 
+vim.opt.cursorline = true
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -17,6 +22,11 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+vim.opt.title = true
+
+vim.opt.autowrite = true
+vim.opt.autowriteall = true
+
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
@@ -25,6 +35,8 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
+
+vim.opt.showmode = false
 
 vim.opt.updatetime = 50
 
