e8da706978b6893e3e5b96a278bfc02a316a5d14
bx2 <burclaf@gmail.com>
2022-12-31 00:08:01 +0100

    update remaps

--- lua/bx2/remap.lua
+++ lua/bx2/remap.lua
@@ -1,12 +1,16 @@
 vim.g.mapleader = ','
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- netrw
+vim.keymap.set('n', '<leader>ee', vim.cmd.Ex)
+
+-- disable Q
 vim.keymap.set("n", "Q", "<nop>")
 
 -- move selection up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- move current line / block with Alt-j/k ala vscode.
+-- fix line joine with J
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- keep the cursor in place when jumping up and down
@@ -17,12 +21,12 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- paste over visual selection
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
 -- copy/paste to system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- paste over visual selection
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- format buffer
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
@@ -32,6 +36,10 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- next/previous buffer
+vim.keymap.set("n", "<leader>.", "<cmd>bn<cr>")
+vim.keymap.set("n", "<leader>m", "<cmd>bp<cr>")
 
 -- replace the current word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
